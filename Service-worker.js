a00b39dd2dfb54c462c095422802f1e4d652bd3e
moshahaly <188513233+moshahaly@users.noreply.github.com>
2025-03-15 21:46:07 +0300

    Add files via upload

---
+++ Service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'car-racing-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/game.js',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => 
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
